da5d8393d7ecd82c270c68598fd7c9e9b1e4351b
Rebecca <rxs6737@rit.edu>
2018-09-25 20:16:58 -0400

    began string parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::Read;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:36201").unwrap();
+
+    let addr = listener.local_addr()
+        .expect("unable to get the local port?");
+
+    println!("Listening on port {}", addr.port());
+
+    for connection in listener.incoming() {
+        match connection {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => panic!(e),
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 32];
+    if let Err(_) = stream.write("Hello from Rust\n".as_bytes()) {
+        return;
+    }
+    println!("connection accepted");
+    loop{
+        if let Ok(read) = stream.read(&mut buffer){
+            let s = String::from_utf8_lossy(&buffer);
+            //println!("{}",s);
+            if read == 0{
+                break;
+            }
+            let mut output = "";
+            match s[0..read].trim(){
+                "a" => output = "Ayy\n",
+                "b" => output = "bark\n",
+                _ => output = s.trim(),
+            }
+            if let Err(_) = stream.write(output.as_bytes()){
+                    break;
+                }
+            
+
+        }else{
+            break;
+        }
+    }
+    println!("disconnected")
+
 }
